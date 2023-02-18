1ed5ddc4d4473b95a6f46444d524abc3efc4ce12
ozz <hosannacchigbu@gmail.com>
2023-02-18 03:41:24 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+{	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	return (1)
+}
