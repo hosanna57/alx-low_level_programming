8d07d332c87d71be2796fe4fd7321ee0966a4c23
ozz <hosannacchigbu@gmail.com>
2023-02-18 04:01:06 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point of program
@@ -6,6 +6,8 @@
  * Return: always 1 (success)
  */
 int main(void)
-{	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-	return (1)
+  char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, msg, sizeof(msg)-1);
+  return (1);
 }
+
